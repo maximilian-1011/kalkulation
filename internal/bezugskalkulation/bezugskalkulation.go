e62d1c6d1640fce3d90a28452d7f53b747db79b3
Maximilian <mbusch1011@gmail.com>
2026-09-04 11:16:29 +0200

    Added /internals/bezugskalkulation. Added bezugskalkulation.go

---
+++ internal/bezugskalkulation/bezugskalkulation.go
@@ -0,0 +1,36 @@
+package bezugskalkulation
+
+import "fmt"
+
+// rabat in % like 5%
+func ZielEinkaufsPreis(listenpreis float64, rabat float64) float64 {
+	if rabat == 0.0 {
+		return listenpreis
+	}
+
+	rabatInDecimal := rabat / 100.0
+
+	rabatInEuro := listenpreis * rabatInDecimal
+
+	zieleinkaufspreis := listenpreis - rabatInEuro
+
+	return zieleinkaufspreis
+}
+
+func BarEinkaufsPreis(zieleinkaufspreis float64, skonto float64) float64 {
+	if skonto == 0.0 {
+		return zieleinkaufspreis
+	}
+
+	skontoInDecimal := skonto / 100.0
+
+	skontoInEuro := zieleinkaufspreis * skontoInDecimal
+
+	bareinkaufspreis := zieleinkaufspreis - skontoInEuro
+
+	return bareinkaufspreis
+}
+
+func BezugsPreis(bareinkaufspreis float64, bezugskosten float64) float64 {
+	return bareinkaufspreis + bezugskosten
+}
